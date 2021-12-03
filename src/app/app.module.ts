be199bd3c9e6b518b6a50bb3067f8b8452d04ad9
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2021-12-02 21:14:19 -0300

    Usando forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -15,17 +17,25 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     CourseListComponent,
     StarComponent,
     ReplacePipe,
-    NavBarComponent
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
-        path: 'courses', component: CourseListComponent
+        path: '**', component: Error404Component
       }
     ])
 
